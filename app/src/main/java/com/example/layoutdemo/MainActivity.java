103e4817a2dff6527829778e93f77ed77dc27240
Shan YunHe <syh95818@apac.arcsoft.corp>
2024-05-28 17:16:30 +0800

    增加linear、relative等新activity，并且设置对应按钮监听跳转对应activity

--- app/src/main/java/com/example/layoutdemo/MainActivity.java
+++ app/src/main/java/com/example/layoutdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.layoutdemo;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void change2LinearLayout(View v) {
+        Log.i("LayoutDemo", "goto linear layout activity");
+        startActivity(new Intent(this, LinearActivity.class));
+    }
+
+    public void change2RelativeLayout(View v) {
+        Log.i("LayoutDemo", "goto relative layout activity");
+        startActivity(new Intent(this, RelativeLayout.class));
+    }
+
+    public void change2TableLayout(View v) {
+        Log.i("LayoutDemo", "goto table layout activity");
+        startActivity(new Intent(this, TableLayout.class));
+    }
+
+    public void change2FrameLayout(View v) {
+        Log.i("LayoutDemo", "goto frame layout activity");
+        startActivity(new Intent(this, FrameLayout.class));
+    }
+
+    public void change2AbsoluteLayout(View v) {
+        Log.i("LayoutDemo", "goto absolute layout activity");
+        startActivity(new Intent(this, AbsoluteLayout.class));
+    }
+
+    public void change2GridLayout(View v) {
+        Log.i("LayoutDemo", "goto grid layout activity");
+        startActivity(new Intent(this, GridLayout.class));
     }
 }
