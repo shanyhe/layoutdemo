a86363e60e6914a07f288ba56e21cc1e2455ee02
Shan YunHe <syh95818@apac.arcsoft.corp>
2024-05-29 16:56:45 +0800

    增加relative部分内容

--- app/src/main/java/com/example/layoutdemo/LinearActivity.java
+++ app/src/main/java/com/example/layoutdemo/LinearActivity.java
@@ -13,12 +13,7 @@ public class LinearActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_linear);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
